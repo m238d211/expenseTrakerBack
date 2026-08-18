96753b8e3491fcca1cda36d002a1002d90d5c1a1
Mahmoud <95576687+m238d211@users.noreply.github.com>
2026-08-18 18:16:30 +0300

    add fix

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,33 @@
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import helmet from 'helmet';
+import type { Request, Response } from 'express';
 import { AppModule } from './app.module';
 import { HttpErrorFilter } from './http-exception.filter';
 
-async function bootstrap() {
+let applicationPromise: Promise<INestApplication> | undefined;
+
+async function createApplication(): Promise<INestApplication> {
   const app = await NestFactory.create(AppModule, { bufferLogs: true });
   app.use(helmet());
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   app.useGlobalFilters(new HttpErrorFilter());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.init();
+  return app;
 }
-void bootstrap();
+
+async function getApplication(): Promise<INestApplication> {
+  applicationPromise ??= createApplication();
+  return applicationPromise;
+}
+
+export default async function handler(request: Request, response: Response) {
+  const app = await getApplication();
+  const expressApplication = app.getHttpAdapter().getInstance() as (req: Request, res: Response) => unknown;
+  return expressApplication(request, response);
+}
+
+if (require.main === module) {
+  void getApplication().then((app) => app.listen(process.env.PORT ?? 3000));
+}
